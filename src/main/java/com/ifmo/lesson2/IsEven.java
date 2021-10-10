35c257d923963c8256dc89f36ee9276addb29961
Denis Cheklov <denis4ik2387@gmail.com>
2021-10-11 01:33:05 +0300

    even

---
+++ src/main/java/com/ifmo/lesson2/IsEven.java
@@ -0,0 +1,24 @@
+package com.ifmo.lesson2;
+
+public class IsEven {
+    /*
+    Создать программу, проверяющую и сообщающую на экран, является ли целое число
+    записанное в переменную n, чётным либо нечётным.
+     */
+    public static void main(String[] args) {
+        int n = 24;
+
+        System.out.println(isEven(n));
+    }
+
+    public static String isEven(int n) {
+        // TODO implement
+        if(n % 2 == 0) {
+
+            // Допустимые строки: "Четное" или "Нечетное"
+
+            return "Четное";
+        } else
+            return "Нечетное";
+    }
+}
