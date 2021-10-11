81630a131b9833ef4f3539fd5cfc432fadd86cbb
Denis Cheklov <denis4ik2387@gmail.com>
2021-10-11 17:33:32 +0300

    55

---
+++ src/main/java/com/ifmo/lesson2/First55.java
@@ -0,0 +1,14 @@
+package com.ifmo.lesson2;
+
+public class First55 {
+    /*
+    Создайте программу, выводящую на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….
+     */
+    public static void main(String[] args) {
+        // TODO implement
+
+        for (int i = 1, j = 1; i <= 55; i++, j+= 2) {
+            System.out.println(j);
+        }
+    }
+}
