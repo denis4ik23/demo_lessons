7497e54f04256ccf3150a5b44d5eb10cd6c674c3
Denis Cheklov <denis4ik2387@gmail.com>
2021-10-13 00:51:47 +0300

    sq2

--- src/main/java/com/ifmo/lesson2/SquareEquation.java
+++ src/main/java/com/ifmo/lesson2/SquareEquation.java
@@ -23,11 +23,11 @@ public class SquareEquation {
      */
     public static double[] squareEquationRoots(double a, double b, double c) {
         // TODO implement
-        double d = b * b - (4 - a * c);
+        double d = b * b - (4 * a * c);
         if(d > 0){
             double[] x12 = new double[2];
-            double x1 = (-1 * b + Math.sqrt(d)) / 2 * a;
-            double x2 = (-1 * b - Math.sqrt(d)) / 2 * a;
+            double x1 = ((-1) * b - Math.sqrt(d)) / (2 * a);
+            double x2 = ((-1) * b + Math.sqrt(d)) / (2 * a);
             x12[0] = x1;
             x12[1] = x2;
             return x12;
