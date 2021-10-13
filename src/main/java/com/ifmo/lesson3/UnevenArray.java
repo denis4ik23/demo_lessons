484f302181754f269ea15fd1a35036319ed53ee6
Denis Cheklov <denis4ik2387@gmail.com>
2021-10-13 03:18:30 +0300

    unenenAr

---
+++ src/main/java/com/ifmo/lesson3/UnevenArray.java
@@ -0,0 +1,33 @@
+package com.ifmo.lesson3;
+
+public class UnevenArray {
+    /*
+    Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку, а затем
+    этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3
+    1)
+     */
+    public static void main(String[] args) {
+        int[] unevenArray = unevenArray();
+
+        // TODO implement
+        for (int i = 0; i < unevenArray.length; i++) {
+            System.out.print(unevenArray[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = unevenArray.length - 1; i >= 0; i--) {
+            System.out.print(unevenArray[i] + " ");
+        }
+
+    }
+
+    public static int[] unevenArray() {
+        // TODO implement
+        int[] uneven = new int[50];
+        for (int i = 0; i < uneven.length; i++) {
+            uneven[i] = i * 2 + 1;
+        }
+
+        return uneven;
+    }
+}
