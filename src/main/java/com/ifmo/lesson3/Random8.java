9fb8a85bb9948510ea304375528f83412b3e5036
Denis Cheklov <denis4ik2387@gmail.com>
2021-10-17 01:10:24 +0300

    rand82

--- src/main/java/com/ifmo/lesson3/Random8.java
+++ src/main/java/com/ifmo/lesson3/Random8.java
@@ -38,8 +38,8 @@ public class Random8 {
 
     public static int[] replaceWithZeros(int[] randomNumbers) {
         // TODO implement
-        for(int i = 1; i < randomNumbers.length; i++){
-            if(randomNumbers[i] % 2 != 0){
+        for(int i = 0; i < randomNumbers.length; i++){
+            if(i % 2 != 0){
                 randomNumbers[i] = 0;
             }
         }
