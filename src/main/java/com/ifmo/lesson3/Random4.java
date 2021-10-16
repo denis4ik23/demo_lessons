01ca9ebdaa8654b2a0ed435fd9f2af4019c9c42a
Denis Cheklov <denis4ik2387@gmail.com>
2021-10-16 21:50:47 +0300

    ran4

---
+++ src/main/java/com/ifmo/lesson3/Random4.java
@@ -0,0 +1,43 @@
+package com.ifmo.lesson3;
+
+import java.util.Random;
+
+import static java.lang.System.*;
+
+public class Random4 {
+    /*
+    Создайте массив из 4 случайных целых чисел из отрезка [10;99], выведите его на экран в
+    строку. Определить и вывести на экран сообщение о том, является ли массив строго
+    возрастающей последовательностью.
+     */
+    public static void main(String[] args) {
+        int[] randomNumbers = randomNumbers();
+
+        // TODO implement
+        for (int i = 0; i < randomNumbers.length; i++) {
+            System.out.print(randomNumbers[i] + " ");
+        }
+        System.out.println(isIncreasingSequence(randomNumbers));
+    }
+
+    public static int[] randomNumbers() {
+        // TODO implement
+        Random random = new Random();
+        int[] ran = new int[4];
+        for (int i = 0; i < ran.length; i++) {
+            ran[i] = random.nextInt(89 + 10);
+        }
+
+        return ran;
+    }
+
+    public static boolean isIncreasingSequence(int[] randomNumbers) {//не правильно
+        // TODO implement
+        for (int i = 1; i < randomNumbers.length; i++) {
+            if(randomNumbers[i] <= randomNumbers[i - 1]){
+        return false;
+            }
+        }
+        return true;
+    }
+}
