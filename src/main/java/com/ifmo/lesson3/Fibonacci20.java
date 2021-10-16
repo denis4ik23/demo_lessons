c4b6e03d6e8927eb3e3041783ff294d8216616b2
Denis Cheklov <denis4ik2387@gmail.com>
2021-10-16 16:32:31 +0300

    fib20

---
+++ src/main/java/com/ifmo/lesson3/Fibonacci20.java
@@ -0,0 +1,29 @@
+package com.ifmo.lesson3;
+
+public class Fibonacci20 {
+    /*
+    Создайте массив из 20-ти первых чисел Фибоначчи и выведите его на экран. Напоминаем,
+    что первый и второй члены последовательности равны единицам, а каждый следующий —
+    сумме двух предыдущих.
+     */
+    public static void main(String[] args) {
+        int[] fibonacciNumbers = fibonacciNumbers();
+
+        // TODO implement
+        System.out.println(fibonacciNumbers);
+    }
+
+    public static int[] fibonacciNumbers() {
+        // TODO implement
+        int[] f = new int[20];
+        f[0] = 1;
+        f[1] = 1;
+        for (int i = 2; i < f.length; i++) {
+            f[i] = f[i - 2] + f[i - 1];
+
+        }
+
+        return f;
+    }
+
+}
