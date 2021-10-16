93f23f52b31d59896519b116a11c5356b4265d15
Denis Cheklov <denis4ik2387@gmail.com>
2021-10-16 17:24:34 +0300

    twoAr

---
+++ src/main/java/com/ifmo/lesson3/TwoArrays.java
@@ -0,0 +1,59 @@
+package com.ifmo.lesson3;
+
+import java.util.Random;
+
+public class TwoArrays {
+    /*
+     Создайте 2 массива из 5 случайных целых чисел из отрезка [0;5] каждый, выведите
+     массивы на экран в двух отдельных строках. Посчитайте среднее арифметическое элементов
+     каждого массива и сообщите, для какого из массивов это значение оказалось больше (либо
+     сообщите, что их средние арифметические равны).
+     */
+    public static void main(String[] args) {
+        int[] randomNumbers1 = randomNumbers();
+        int[] randomNumbers2 = randomNumbers();
+
+        // TODO implement
+        for (int i = 0; i < randomNumbers1.length; i++) {
+            System.out.print(randomNumbers1[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < randomNumbers2.length; i++) {
+            System.out.print(randomNumbers2[i]);
+        }
+        System.out.println();
+
+        int average1 = average(randomNumbers1);
+        int average2 = average(randomNumbers2);
+
+        // TODO implement
+        if (average1 > average2){
+            System.out.println("Среднее арифметическое элементов массива randomNumbers1 больше чем у массива randomNumbers2");
+        } else if (average2 > average1){
+            System.out.println("Среднее арифметическое элементов массива randomNumbers2 больше чем у массива randomNumbers1");
+        } else if (average1 == average2){
+            System.out.println("Среднее арифметическое элементов массива randomNumbers1 и randomNumbers2 равны");
+        }
+    }
+
+    public static int[] randomNumbers() {
+        // TODO implement
+        Random random = new Random();
+        int[] rn = new int[5];
+        for (int i = 0; i < 5; i++) {
+            rn[i] = random.nextInt(5);
+        }
+
+        return rn;
+    }
+
+    public static int average(int[] randomNumbers) {
+        // TODO implement
+        int sum = 0;
+        for (int randomNumber : randomNumbers) {
+            sum = sum + randomNumber;
+        }
+        return sum;
+    }
+}
